1bfb7ffa91d8e1cd4cc9e292371ba9d2d0a312e9
dj-karthick <karthickallancherry@gmail.com>
2026-06-20 09:41:02 +0530

    Completed the Week 6 Assignments.

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -12,7 +12,13 @@ export const Assignment3 = () => {
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(() => {
+        let totalValue = 0;
+        items.forEach((todo) => {
+            totalValue += todo.value;
+        })
+        return totalValue;
+    }, [items]);
     // Your code ends here
     return (
         <div>
--- week-6/3-use-ref/src/components/Assignment2.jsx
+++ week-6/3-use-ref/src/components/Assignment2.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 
 // Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
 
 export function Assignment2() {
     const [, forceRender] = useState(0);
+
+    const noOfRerender = useRef(0);
+    noOfRerender.current = noOfRerender.current + 1;
 
     const handleReRender = () => {
         // Update state to force re-render
@@ -12,7 +15,7 @@ export function Assignment2() {
 
     return (
         <div>
-            <p>This component has rendered {0} times.</p>
+            <p>This component has rendered {noOfRerender.current} times.</p>
             <button onClick={handleReRender}>Force Re-render</button>
         </div>
     );
